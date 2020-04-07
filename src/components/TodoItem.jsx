7b0a5497b23a744ca022200533e1a8292d5ef6d6
MadhawaJayagoda <46900597+MadhawaJayagoda@users.noreply.github.com>
2020-04-07 22:06:36 +0530

    update 03

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,22 +1,6 @@
 import React, {Component} from 'react';
 import './TodoItem.css';
 
-/*
-*
-*
-* ----------------------------------------  Student Details --------------------------------------
-
-Application Frameworks : Mid Exam Assignment
-
- > Student Registration Number :  IT17184304
- > Student Name                :  Jayagoda N.M.
-
- (  SLIIT 3rd Year 1st semester Software Engineering Weekday Batch - Y3S1.20.1  )
- *
- *
- *
-* */
-
 class TodoItem extends Component {
     render() {
         const { isComplete } = this.props;
